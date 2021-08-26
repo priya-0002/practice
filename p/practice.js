63f0e4c6a2d3b691cdc38fd46b1a11c254e17d93
priya-0002 <priya.md0002@gmail.com>
2021-08-26 18:48:54 +0530

    xyz

---
+++ p/practice.js
@@ -0,0 +1,17 @@
+const num1 = 5;
+const num2 = 3;
+
+// add two numbers
+const sum = num1 + num2;
+
+//random
+const a= Math.random();
+console.log(a);
+
+//factorial
+
+let factorial=1;
+for(i=1;i<=number;i++){
+    factorial*=i;
+}
+console.log(factorial)
