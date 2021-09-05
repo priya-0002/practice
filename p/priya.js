4e4a4f907e94aa7571d6e0121ca62ff6f71dbf6b
priya-0002 <priya.md0002@gmail.com>
2021-09-05 11:50:34 +0530

    xyz

--- p/priya.js
+++ p/priya.js
@@ -1,2 +1,30 @@
+const num1 = 5;
+const num2 = 3;
 
+// add two numbers
+const sum = num1 + num2;
+console.log(sum)
+
+//random
+const a= Math.random();
+console.log(a);
+
+var arr=[1,2,3]
+var addition=arr.reduce((a,i)=>{
+    return a+i
+});
+console.log(addition)
+
+function fun() {
+    return
+    
+      name: "Full Stack Tutorials";
+    
+  }
+
+  console.log(fun());
+
+
+  let a = 10;
+  var a = 20;
   console.log(a);
